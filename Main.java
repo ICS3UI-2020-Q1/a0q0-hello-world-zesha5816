ca7a6a91dedbb9de2cfcdd2033fd7ba00bb8ba7d
zesha5816 <zesha5816@wrdsb.ca>
2020-09-11 15:55:23 +0000

    Finished Hello World! program

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Print "Hello world!"" to the console
+    System.out.println("Hello World!"); 
   }
 }
